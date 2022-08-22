a6e4cd32588c45fa05def28009d7d4ee82347d18
Vishal Kumar <vishalkrnda@gmail.com>
2022-08-22 08:50:43 +0530

    Commit

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+var btc = document.getElementById("bitcoin");
+var ltc = document.getElementById("litecoin");
+var eth = document.getElementById("ethereum");
+var doge = document.getElementById("dogecoin");
+
+var liveprice = {
+    "async": true,
+    "scroosDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+
+    "method": "GET",
+    "headers": {}
+}
+
+$.ajax(liveprice).done(function (response){
+    btc.innerHTML = response.bitcoin.usd;
+    ltc.innerHTML = response.litecoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+
+});
+
 const dropmenu = document.querySelector('ul.dropdown-menu');
 const dropbtn = document.querySelector('.dropdown-btn');
 
